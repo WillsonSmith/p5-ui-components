b3471fe1aa3abe0768510b4cde8b02a68c199598
Willson Smith <me@willsonsmith.com>
2021-04-24 16:24:18 -0400

    a bunch of stuff to use svgs for background, handle resize etc

--- components/message/p-message.js
+++ components/message/p-message.js
@@ -1,55 +1,164 @@
-import {LitElement, html, css} from 'lit-element';
+import { LitElement, html, css } from "lit-element";
 
 class PMessage extends LitElement {
-  static get properties() {
-    return {
-      image: {attribute: 'image', type: String}
-    }
-  }
-
   static get styles() {
     return css`
       @font-face {
-        font-family: 'Persona 5';
-        src: url('../../fonts/p5hatty.ttf');
+        font-family: "Persona 5";
+        src: url("../../fonts/p5hatty.ttf");
       }
       :host {
         --message-matting: rgba(255, 255, 255, 1);
         --message-background: rgba(0, 0, 0, 1);
         --message-text-color: rgba(255, 255, 255, 1);
-        --font-family: 'Persona 5';
+        --font-family: "Persona 5";
         --matting-padding: 0.2rem;
         --message-padding: 1rem;
+        display: block;
+        box-sizing: border-box;
+      }
+
+      :host(*),
+      :host(*::before),
+      :host(*::after) {
+        box-sizing: inherit;
       }
 
       .Message {
-        background: var(--message-matting);
-        font-family: 'Persona 5';
-        padding: var(--matting-padding);
-        transform: skew(359deg, 359deg);
+        position: relative;
+      }
+      .Message::before {
+        content: "";
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+
+        transform: rotateY(-1.54deg) skewX(-12.93deg);
       }
       .Content {
-        background: var(--message-background);
+        position: relative;
         color: var(--message-text-color);
+      }
+
+      .Content-Container {
+        padding: var(--matting-padding);
+      }
+
+      .Content::before {
+        content: "";
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+      }
+
+      .Text {
+        position: relative;
         padding: var(--message-padding);
+      }
+
+      .Message svg {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        fill: var(--message-matting);
+
+        overflow: visible;
+      }
+      .Content svg {
+        fill: var(--message-background);
       }
     `;
   }
 
+  static get properties() {
+    return {
+      image: { attribute: "image", type: String },
+      width: Number,
+      height: Number,
+      contentWidth: Number,
+      contentHeight: Number,
+    };
+  }
+
+  resize(width, height) {
+    if (this.resizeTimeout) window.cancelAnimationFrame(this.resizeTimeout);
+    this.resizeTimeout = window.requestAnimationFrame(() => {
+      this.width = width;
+      this.height = height;
+    });
+  }
+
+  resizeContent(width, height) {
+    if (this.resizeTimeout2) window.cancelAnimationFrame(this.resizeTimeout2);
+    this.resizeTimeout2 = window.requestAnimationFrame(() => {
+      this.contentWidth = width;
+      this.contentHeight = height;
+    });
+  }
+
   constructor() {
     super();
+    this.width = 0;
+    this.height = 0;
+    this.contentWidth = 0;
+    this.contentHeight = 0;
+    this.resizeObserver = new ResizeObserver((entries) => {
+      for (const entry of entries) {
+        const { width, height } = entry.contentRect;
+        if (entry.target === this.contentNode) {
+          this.resizeContent(width, height);
+        }
+        if (entry.target === this) {
+          this.resize(width, height);
+        }
+      }
+    });
+
+    this.resize = this.resize.bind(this);
+    this.resizeContent = this.resizeContent.bind(this);
+  }
+
+  firstUpdated() {
+    this.contentNode = this.shadowRoot.querySelector(".Content");
+    this.resizeObserver.observe(this.contentNode);
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.resizeObserver.observe(this);
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
   }
 
   render() {
-
     return html`
       <div class="Message">
-        <div class="Content">
-          <slot></slot>
+        <svg xmlns="http://www.w3.org/2000/svg">
+          <path
+            d=${`M0 0 L ${this.width} 0 L ${this.width} ${this.height} L 0 ${this.height}`}
+          />
+        </svg>
+        <div class="Content-Container">
+          <div class="Content">
+            <svg xmlns="http://www.w3.org/2000/svg">
+              <path
+                d=${`M0 0 L ${this.contentWidth} 0 L ${this.contentWidth} ${this.contentHeight} L 0 ${this.contentHeight}`}
+              />
+            </svg>
+            <div class="Text">
+              <slot></slot>
+            </div>
+          </div>
         </div>
       </div>
     `;
   }
 }
 
-customElements.define('p-message', PMessage);
+customElements.define("p-message", PMessage);
--- stories/p-message.stories.js
+++ stories/p-message.stories.js
@@ -1,21 +1,29 @@
-import { html } from 'lit-html';
-import '../components/message/p-message.js';
+import { html } from "lit-html";
+import "../components/message/p-message.js";
 
 /**
  * Primary UI component for user interaction
  */
 
 export default {
-  title: 'p-message',
-  component: 'p-message',
-}
+  title: "p-message",
+  component: "p-message",
+};
 
 const Template = () => {
   return html`
-    <div style='width: 100%; height: 800px; background: #B40001; padding: 1rem; box-sizing: border-box;'>
+    <style>
+      body {
+        font-family: sans-serif;
+        font-size: 1.6rem;
+      }
+    </style>
+    <div
+      style="width: 100%; height: 800px; background: #B40001; padding: 1rem; box-sizing: border-box;"
+    >
       <p-message>Some text here</p-message>
     </div>
   `;
-}
+};
 
 export const Basic = Template.bind({});
